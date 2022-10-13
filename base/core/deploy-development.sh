1b34a01519d32abfc8151f631b59c5128ed724e4
Melvin Sommer <melvin@coeus-it.com>
2022-10-13 05:05:22 +0200

    up

--- base/core/deploy-development.sh
+++ base/core/deploy-development.sh
@@ -1,8 +1,10 @@
 #!bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$(dirname "$0")"
-[ -d "./grace" ] && cd grace && git pull && cd ..
-[ ! -d "./grace" ] && git clone https://github.com/coeusit/grace.git
-docker-compose -p coeus_development -f "./docker-compose.development.yml" up -d
+[ -d "$SCRIPT_DIR/grace" ] && cd $SCRIPT_DIR/grace && git pull && cd ..
+[ ! -d "$SCRIPT_DIR/grace" ] && git clone https://github.com/coeusit/grace.git $SCRIPT_DIR/grace
+source $SCRIPT_DIR/docker-conf/.env.development
+docker-compose -p $COMPOSE_PROJECT_NAME -f "$SCRIPT_DIR/docker-compose.development.yml" up -d
 unameOut="$(uname -s)"
 case "${unameOut}" in
     Linux*)     machine=Linux;;
