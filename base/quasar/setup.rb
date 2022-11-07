a7e31d33e5c8ea4b2372dad734e7d6154cf4cd28
Melvin Sommer <melvin@coeus-it.com>
2022-11-07 18:59:32 +0100

    up

--- base/quasar/setup.rb
+++ base/quasar/setup.rb
@@ -25,10 +25,11 @@ if conn_method == 'websocket'
   system("yarn add #{plugins.join(' ')}")
   FileUtils.cp_r("#{tmpl_path}/.","./")
   src = File.read('./src/boot/websocket.js')
-  src.gsub!('#domain', ws_domain)
-  src.gsub!('#port', ws_port)
+  src.gsub!('#domain', ws_domain.to_s)
+  src.gsub!('#port', ws_port.to_s)
   File.open('./src/boot/websocket.js', 'w') {|f| f.puts src }
   @logger.info 'Quasar package installed'
+  @logger.info 'Remember to enable the Notify and Cookies plugins in quasar.config.js'
 else
   @logger.info "The REST client is under revision"
 end
